8a2c736ab98063eb769596224d71923674d1c74c
Ishita patil <patilishita2007@gmail.com>
2026-03-15 13:30:30 +0530

    Add files via upload

---
+++ day16.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+void main() {
+  print("Simple Calculator");
+
+  stdout.write("Enter first number: ");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter second number: ");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  print("\nChoose an operation:");
+  print("1. Addition");
+  print("2. Subtraction");
+  print("3. Multiplication");
+  print("4. Division");
+
+  stdout.write("Enter your choice (1-4): ");
+  int choice = int.parse(stdin.readLineSync()!);
+
+  switch (choice) {
+    case 1:
+      print("Result = ${num1 + num2}");
+      break;
+
+    case 2:
+      print("Result = ${num1 - num2}");
+      break;
+
+    case 3:
+      print("Result = ${num1 * num2}");
+      break;
+
+    case 4:
+      if (num2 != 0) {
+        print("Result = ${num1 / num2}");
+      } else {
+        print("Cannot divide by zero");
+      }
+      break;
+
+    default:
+      print("Invalid choice");
+  }
+}
